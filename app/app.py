0499b416afb6e28f781f15c5ab7213f384bd3079
unknown <okuda1202>
2023-01-24 23:11:51 +0900

    Initial Commit

---
+++ app/app.py
@@ -0,0 +1,41 @@
+from flask import Flask,render_template,request
+from models.models import OnegaiContent
+#以下を追加
+from models.database import db_session
+from datetime import datetime
+#追加終わり
+
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+    name = request.args.get("name")
+    all_onegai = OnegaiContent.query.all()
+    return render_template("index.html",name=name,all_onegai=all_onegai)
+
+
+@app.route("/index",methods=["post"])
+def post():
+    name = request.form["name"]
+    all_onegai = OnegaiContent.query.all()
+    return render_template("index.html", name=name, all_onegai=all_onegai)
+
+
+#以下を追加
+@app.route("/add",methods=["post"])
+def add():
+    title = request.form["title"]
+    body = request.form["body"]
+    content = OnegaiContent(title,body,datetime.now())
+    db_session.add(content)
+    db_session.commit()
+   
+
+    return index()
+#追加終わり
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
